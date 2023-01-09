84bd01ceaf5422d1802a9d3569ba6db12ab6e9ad
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-09 22:48:00 +0100

    suma różnych od 0

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("insert number");
+
+int number = int.Parse(Console.ReadLine());
+
+int zero = 0;
+
+int sum = 0;
+
+while (number != zero)
+{
+    sum += number;
+    number++;
+    number = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(sum);
