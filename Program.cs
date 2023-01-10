bdd147f62a199f4a9da7c0143ff93fa23d088061
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-10 22:32:14 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -61,3 +61,15 @@ Console.WriteLine(liczba);
 
 
 
+//int a, b;
+//int suma = 0;
+//
+//for (int i = 1; i < 10; i++)
+//{
+//    for(int j = 1; j < 10; j++)
+//    {
+//        suma = i * j;
+//        Console.WriteLine(i+" "+j+" = "+suma );
+//    }
+//
+//}
