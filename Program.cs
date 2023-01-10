270f1baf4691f57e8c4b84d55c3e9346372b3788
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-10 22:28:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,3 +13,11 @@ while (number != zero)
     number = int.Parse(Console.ReadLine());
 }
 Console.WriteLine(sum);
+
+
+/*Napisać program, który pobiera od użytkownika liczbę całkowitą dodatnią,
+a następnie wyświetla na ekranie kolejno wszystkie liczby niepatrzyste nie większe od
+podanej liczby. Przykład, dla 15 program powinien wyświetlić 1, 3, 5, 7, 9, 11, 13,
+15.
+*/
+
