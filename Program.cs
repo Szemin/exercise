73ab4ed0d88c29f66f6d7f3c5af2be88cd1ac4af
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-12 22:47:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,14 +5,19 @@
         static void Main(string[] args)
         {
           
+            
             Human human1 = new Human();
+            Human human2 = new Human();
 
             human1.name = "Rick";
             human1.age = 69;
 
+            human1.name = "Morty";
+            human1.age = 16;
+
             human1.Eat();
             human1.Sleep();
-            
+
             Console.ReadKey();
         }
     }
