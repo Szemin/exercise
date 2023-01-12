e1188578d5ce427f3dee7b08054ce83fb8573f4f
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-12 22:54:56 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,35 +8,38 @@
               //obiekt to instancja klasy, a klasa może być używana przez plan(blueprint) do tworzenia obiektów
             // obiekty mogą mieć pola i metody - pola(fields) to to co obiekt zawiera, metoda to to co obiekt może zrobić
             
-            Human human1 = new Human();
+           Human human1 = new Human();
             Human human2 = new Human();
 
             human1.name = "Rick";
             human1.age = 69;
 
-            human1.name = "Morty";
-            human1.age = 16;
+            human2.name = "Morty";
+            human2.age = 16;
 
             human1.Eat();
             human1.Sleep();
+
+            human2.Eat();
+            human2.Sleep();
 
             Console.ReadKey();
         }
     }
    public class Human
     {
-       public String name;
-       public int age;
+       public String name;  //obiekt string
+       public int age;     // obiekt int
 
        public void Eat()
         {
-            Console.WriteLine(name + "is eating");    
+            Console.WriteLine($"{name} is eating");    
 
             
         }
       public void Sleep()
         {
-            Console.WriteLine(name + "is sleeping");
+            Console.WriteLine($"{name} is sleeping");
 
 
         }
