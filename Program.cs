3576007b1b308b864d14b0a6b1f0b33aad218c69
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-12 22:39:10 +0100

    Obiekty

--- Program.cs
+++ Program.cs
@@ -1,75 +1,10 @@
-﻿Console.WriteLine("insert number");
-
-int number = int.Parse(Console.ReadLine());
-
-int zero = 0;
-
-int sum = 0;
-
-while (number != zero)
+﻿namespace ConsoleApp10
 {
-    sum += number;
-    number++;
-    number = int.Parse(Console.ReadLine());
-}
-Console.WriteLine(sum);
-
-
-/*Napisać program, który pobiera od użytkownika liczbę całkowitą dodatnią,
-a następnie wyświetla na ekranie kolejno wszystkie liczby niepatrzyste nie większe od
-podanej liczby. Przykład, dla 15 program powinien wyświetlić 1, 3, 5, 7, 9, 11, 13,
-15.
-*/
-
-Console.WriteLine("Insert number");
-
-int number = int.Parse(Console.ReadLine());
-
-for (int i = 0; i < number; i++)
-{
-    if (i % 2 == 1)
+    internal class Program
     {
-        Console.WriteLine(i);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
-}
-
-
-
-/*for
-przedział 0 - 99
-    n = 5;
-oblicza średnią wszystkich liczb
-*/
-
-
-for (int i = 0; i < n; i++)
-{
-    int rand_num = random.Next(0, 99);
-   liczba = rand_num;
-   Console.WriteLine(rand_num);
-  sr += rand_num/n;
-
-
-Console.WriteLine("\n " + sr);
-Console.WriteLine(liczba);
-}
-
-
-
-//przedział 1 do 100 czyli maksymalnie 10x10 
-//podwójna pętla for
-
-
-
-int a, b;
-int suma = 0;
-
-for (int i = 1; i < 10; i++)
-{
-   for(int j = 1; j < 10; j++)
-   {
-        suma = i * j;
-       Console.WriteLine(i+" "+j+" = "+suma );
-   }
-
 }
